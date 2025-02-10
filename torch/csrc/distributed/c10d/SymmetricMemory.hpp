76dd06ca6defb094e1a4dabfb4307d5bb9fbdf01
Yifu Wang <yifu@fb.com>
2025-02-10 15:27:44 -0800

    [NOT FOR LANDING] experimental NVSHMEM integration
    ghstack-source-id: dc491e42892ca3f061dcef4029a8e73749cb0c2f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146593

--- torch/csrc/distributed/c10d/SymmetricMemory.hpp
+++ torch/csrc/distributed/c10d/SymmetricMemory.hpp
@@ -71,6 +71,9 @@ class TORCH_API SymmetricMemory : public c10::intrusive_ptr_target {
 
   virtual int get_rank() = 0;
   virtual int get_world_size() = 0;
+  virtual int* get_rank_to_global_rank_dev() {
+    TORCH_CHECK(false, "NYI");
+  };
 };
 
 class SymmetricMemoryAllocator : public c10::intrusive_ptr_target {
